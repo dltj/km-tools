5999ca6a0fa9b790dba5d299e0256f17c32503ea
Peter Murray <jester@dltj.org>
2021-05-15 17:42:58 -0400

    Refactor out credentials

---
+++ action/Twitter.py
@@ -0,0 +1,44 @@
+import sqlite3
+import click
+from TwitterAPI import TwitterAPI
+
+
+@click.command()
+@click.pass_obj
+def twitter(details):
+    twiter_api = TwitterAPI(
+        details.config.twitter.consumer_key,
+        details.config.twitter.consumer_secret,
+        details.config.twitter.access_token_key,
+        details.config.twitter.access_token_secret,
+    )
+
+    # Get the number of characters a Twitter-shortened URL will take up
+    r = twiter_api.request("help/configuration")
+    twitter_config = r.json()
+    short_url_length = twitter_config["short_url_length_https"]
+
+    db_con = sqlite3.connect("pinboard.db")
+    db_con.row_factory = sqlite3.Row
+    search_cur = db_con.cursor()
+    for row in search_cur.execute(
+        "SELECT * FROM posts WHERE posted_to_twitter=0 ORDER BY time"
+    ):
+        url_length = short_url_length if len(row[1]) > short_url_length else len(row[1])
+        meta_text = 4
+        text_length = 280 - url_length - meta_text
+        tweet_text = f"🔖 {row['description'][:text_length]} {row['href']}"
+        if not details.dry_run:
+            r = twiter_api.request("statuses/update", {"status": tweet_text})
+            if r.status_code == 200:
+                click.echo("SUCCESS")
+                click.echo(row["hash"])
+                update_cur = db_con.cursor()
+                update_cur.execute(
+                    "UPDATE posts SET posted_to_twitter=1 WHERE hash=?", [row["hash"]]
+                )
+                db_con.commit()
+            else:
+                click.echo("PROBLEM: " + r.text)
+        else:
+            click.echo(f"Would have tweeted: {tweet_text}")
---
+++ kmtools.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""Knowledge Management CLI."""
+
+import click
+from omegaconf import OmegaConf
+from action import Twitter
+
+
+class Details:  # pylint: disable=too-few-public-methods
+    """Application-specific context"""
+
+    def __init__(self, dry_run=False, config=None):
+        self.dry_run = dry_run
+        self.config = config
+
+
+@click.group(context_settings={"help_option_names": ["-h", "--help"]})
+@click.option("--dry-run", is_flag=True)
+@click.pass_context
+def cli(ctx, dry_run):
+    """Root command line function"""
+    config = OmegaConf.load("config.yml")
+    OmegaConf.set_readonly(config, True)
+
+    ctx.obj = Details(dry_run, config)
+
+
+# Register commands
+cli.add_command(Twitter.twitter)
+
+# pylint: disable=no-value-for-parameter
+if __name__ == "__main__":
+    cli()
