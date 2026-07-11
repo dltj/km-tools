3c01c3052724fe4441d6f1d1434634a35cb669a0
Peter Murray <jester@dltj.org>
2026-07-11 11:08:34 -0400

    Make database path relative to project directory home

--- kmtools/util/database.py
+++ kmtools/util/database.py
@@ -1,8 +1,11 @@
+from pathlib import Path
+
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-# Define database URI and create engine
-engine: Engine = create_engine("sqlite:///kmtools.sqlite3")
+# Define database URI and create engine.
+DB_PATH = Path(__file__).resolve().parents[2] / "kmtools.sqlite3"
+engine: Engine = create_engine(f"sqlite:///{DB_PATH}")
 
 # Create a session factory bound to this engine
 SessionFactory = sessionmaker(bind=engine)
